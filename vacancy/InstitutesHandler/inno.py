c676d21715ea06a1648bb50e5e992c21fed470eb
rtix <radif.tazetdinov@mail.ru>
2019-03-13 21:23:50 +0300

    inno.py module designed

--- vacancy/InstitutesHandler/inno.py
+++ vacancy/InstitutesHandler/inno.py
@@ -1,5 +1,27 @@
 # {'title':str, 'salary':str, 'url':str}
+import requests
+from bs4 import BeautifulSoup
 
 
 def get_vacancy():
-    pass
+    url = r"https://university.innopolis.ru/about/job/"
+
+    vacancy_class = r"panel panel-default"
+    title_class = r"accordion-toggle"
+    salary_class = r"pull-right"
+    url_class = r"btn btn-color-1"
+
+    result = []
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    vacancy = soup.find_all('div', {'class':vacancy_class})
+    for vac in vacancy:
+        result_item = {}
+        result_item['title'] = vac.find('a', 
+                                        {'class':title_class}).string.strip()
+        result_item['salary'] = vac.find('p', 
+                                        {'class':salary_class}).string.strip()
+        if result_item['salary']==r'з/п не указана':
+            result_item['salary'] = None
+        result_item['url'] = vac.find('a', {'class':url_class})["href"]
+        result.append(result_item)
+    return result
