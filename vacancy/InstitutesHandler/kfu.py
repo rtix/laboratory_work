44bfe966babcdd819761962a2aceca206acb6aa4
Gippp <43097191+Gippp@users.noreply.github.com>
2019-03-14 12:37:26 +0300

    Update kfu.py

--- vacancy/InstitutesHandler/kfu.py
+++ vacancy/InstitutesHandler/kfu.py
@@ -1,5 +1,20 @@
 # {'title':str, 'salary':str, 'url':str}
+import requests
+import re
+from bs4 import BeautifulSoup
 
 
 def get_vacancy():
-    pass
+    url="https://career.kpfu.ru/vacancy/kfu"
+    bs=BeautifulSoup(requests.get(url).text,'html.parser')
+    url1="https://career.kpfu.ru"
+    vacancy=[]
+
+    vac=bs.findAll('div',{'class':'vacancyItem header'})
+    for i in vac:
+        res={}
+        res['title']=i.find('div',{'class':'vacancyItem-name'}).text
+        res['salary']=i.find(text=re.compile(r'от \d{1,9}.*'))
+        res['url']=url1+i.find('div',{'class':'vacancyItem-name'}).find('a')['href']
+        vacancy.append(res)
+    return vacancy
