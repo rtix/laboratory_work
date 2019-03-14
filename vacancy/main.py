1d2045956783a66a37c644b8eda2a7f6c19bacbd
rtix <radif.tazetdinov@mail.ru>
2019-03-14 20:07:40 +0300

    Little improvement of code

--- vacancy/main.py
+++ vacancy/main.py
@@ -1,16 +1,6 @@
 import InstitutesHandler as ih
 import re
 
-def main():
-    kfu = ih.kfu.get_vacancy() 
-    print("Казанский (Приволжский) федеральный университет") 
-    for i, k in enumerate(kfu, 1): 
-        print(str(i)+'.', k['title'], k['salary'], k['url'], sep='\n')
-    
-    inno=ih.inno.get_vacancy()
-    print("Университет Иннополис")
-    for i,k in enumerate(inno,1):
-        print(str(i)+'.',k['title'], k['salary'], k['url'], sep='\n')
 
 def compare(list,word):
     res=[]
@@ -19,21 +9,26 @@ def compare(list,word):
             res.append(i)
     return res
 
+
 def search(word):
     kfu = ih.kfu.get_vacancy()
-    inno=ih.inno.get_vacancy() 
+    inno = ih.inno.get_vacancy() 
     
-    kfu_res=compare(kfu,word)
-    inno_res=compare(inno,word)
-    
-    print("\nКазанский (Приволжский) федеральный университет")
-    for i, k in enumerate(kfu_res, 1): 
-        print(str(i)+'.', k['title'], k['salary'], k['url'], sep='\n')
+    result = {"Казанский (Приволжский) федеральный университет:":compare(kfu,word),
+                "Университет Иннополис:":compare(inno,word)}
 
-    print("\nУниверситет Иннополис")
-    for i,k in enumerate(inno_res,1):
-        print(str(i)+'.',k['title'], k['salary'], k['url'], sep='\n')
+    return result
+    
+
+def main():
+    result = search(input("Введите искомую вакансию"
+                            "(Enter, если нет предпочтений):"))
+    for uni in result.items():
+        print(uni[0])
+        for i, k in enumerate(uni[1], 1): 
+            print(str(i)+'.', k['title'], k['salary'], k['url'], sep='\n')
+        print('\n')
+
 
 if __name__=='__main__':
-    # main()
-    search("программисТ")   
+    main()
