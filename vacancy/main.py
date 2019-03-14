e5040a3c0dd2b4c1a75aadec6e5e1ff3ecff398a
Gippp <wood.n96@gmail.com>
2019-03-14 19:32:23 +0300

    Пока вот так, жду остальные вузы

--- vacancy/main.py
+++ vacancy/main.py
@@ -1,9 +1,39 @@
 import InstitutesHandler as ih
-
+import re
 
 def main():
-    pass
+    kfu = ih.kfu.get_vacancy() 
+    print("Казанский (Приволжский) федеральный университет") 
+    for i, k in enumerate(kfu, 1): 
+        print(str(i)+'.', k['title'], k['salary'], k['url'], sep='\n')
+    
+    inno=ih.inno.get_vacancy()
+    print("Университет Иннополис")
+    for i,k in enumerate(inno,1):
+        print(str(i)+'.',k['title'], k['salary'], k['url'], sep='\n')
 
+def compare(list,word):
+    res=[]
+    for i in list:
+        if re.search(r'(?i){}'.format(word),i['title']):
+            res.append(i)
+    return res
+
+def search(word):
+    kfu = ih.kfu.get_vacancy()
+    inno=ih.inno.get_vacancy() 
+    
+    kfu_res=compare(kfu,word)
+    inno_res=compare(inno,word)
+    
+    print("Казанский (Приволжский) федеральный университет")
+    for i, k in enumerate(kfu_res, 1): 
+        print(str(i)+'.', k['title'], k['salary'], k['url'], sep='\n')
+
+    print("Университет Иннополис")
+    for i,k in enumerate(inno_res,1):
+        print(str(i)+'.',k['title'], k['salary'], k['url'], sep='\n')
 
 if __name__=='__main__':
-    main()
+    # main()
+    search("php")
